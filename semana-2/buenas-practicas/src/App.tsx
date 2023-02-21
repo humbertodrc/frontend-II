00c629e6283d7d93e6c3832c483c70b917af9bd8
humbertodrc <humbertorivero9@gmail.com>
2023-02-21 20:20:29 -0300

    Fix main container

--- semana-2/buenas-practicas/src/App.tsx
+++ semana-2/buenas-practicas/src/App.tsx
@@ -4,7 +4,7 @@ import { Header } from './components/Header';
 
 function App() {
 	return (
-		<div className='bg-black  w-full p-8'>
+		<div className='bg-black min-h-screen w-full p-8'>
       <Header />
       <CardList />
 		</div>
