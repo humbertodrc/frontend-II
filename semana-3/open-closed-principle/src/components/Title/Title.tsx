972b680e2594e8cc0f12d21f22107e8a3dc60de7
humbertodrc <humbertorivero9@gmail.com>
2023-02-27 18:06:15 -0300

    Fix message open-close

--- semana-3/open-closed-principle/src/components/Title/Title.tsx
+++ semana-3/open-closed-principle/src/components/Title/Title.tsx
@@ -8,7 +8,7 @@ export interface TitleProps {
 	onClick?: () => void;
 }
 
-// No cumple con el principio Open-Closed ya que se debe modificar el componente para agregar un nuevo tipo de título con button o link
+// No cumple con el principio Open-Closed ya que se debe modificar el componente para agregar un nuevo tipo de título con button o link, crear un componente WhithButtonTitle y WithLinkTitle al ser mas específicos y no tener que modificar el componente Title
 
 const Title: React.FC<TitleProps> = ({title, type, href, buttonText, onClick}) => {
 	return <div className={styles.title}>
