1e0ca3ddd1ec41bfcb65ed364800489ccb7bbd0d
humbertodrc <humbertorivero9@gmail.com>
2023-03-26 13:27:29 -0300

    add devdependencies for jest and RTL

--- semana-7/test-customs-hooks/jest.config.cjs
+++ semana-7/test-customs-hooks/jest.config.cjs
@@ -1,3 +1,7 @@
 module.exports = {
-  testEnvironment: 'jest-environment-jsdom',
-}
+	testEnvironment: "jest-environment-jsdom",
+	// setupFiles: ['./jest.setup.js']
+	moduleNameMapper: {
+    "\\.(css|sass)$": "identity-obj-proxy",
+  },
+};
